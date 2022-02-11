bbf8c5d7a3dcba0790c68a5064160ef7518e41c0
SULLENGOD <luis.d.sullen@outlook.com>
2022-02-11 13:24:11 -0600

    Mod cearUsuario

--- src/components/CrearUsuario/CrearUsuario.jsx
+++ src/components/CrearUsuario/CrearUsuario.jsx
@@ -29,12 +29,12 @@ const CrearUsuario = () => {
       email: payload.email,
       pass: payload.pass,
       name: payload.name,
-      //  last_name: payload.last_name
-      // brd_date: event.target[4].value,
-      // hobby: event.target[5].value,
-      // puesto: event.target[6].value,
-      // habilidad: event.target[7].value,
-      // img: event.target[8].value,
+      last_name: payload.last_name,
+      brd_date: payload.brd_date,
+      hobby: payload.hobby,
+      puesto: payload.puesto,
+      habilidad: payload.habilidad,
+      img: payload.img
     };
 
     setUser(usuarioCreado);
@@ -49,46 +49,40 @@ const CrearUsuario = () => {
     watch('name')
     );
 
-  return (
-    <>
-      <div className="container">
-        <br />
-        <br />
-        <div className="card" id="cardForm">
-          <h1 className="text-center">Ingresa tus Datos:</h1>
-          <form onSubmit={handleSubmit(createUser)}>
-            <div className="mb-3">
-              <label className="form-label">Email</label>
-              <div className="form-floating mb-3">
-              <input
-                {...register("email", {
-                  required: true,
-                  maxLength: 100,
-                  pattern:
-                    /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})/,
-                })}
-                className="form-control"
-                placeholder="email"
-              />
-              {errors?.email?.type === "required" && (
-                <p className="error">Este campo es requerido</p>
-              )}
-              {errors?.email?.type === "pattern" && (
-                <p className="error">Ingresa un email valido</p>
-              )}
-              <label for="floatingInput">ejemplo@ejem.com</label>
+    return (
+      <>
+        <div className="container">
+          <br />
+          <br />
+          <div className="card" id="cardForm">
+            <h1>Ingresa tus Datos:</h1>
+            <form onSubmit={handleSubmit(createUser)}>
+              <div className="mb-3">
+                <label className="form-label">Email</label>
+                <input
+                  {...register("email", {
+                    required: true,
+                    maxLength: 100,
+                    pattern:
+                      /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})/,
+                  })}
+                  className="form-control"
+                />
+                {errors?.email?.type === "required" && (
+                  <p className="error">Este campo es requerido</p>
+                )}
+                {errors?.email?.type === "pattern" && (
+                  <p className="error">Ingresa un email valido</p>
+                )}
               </div>
-            </div>
-            <div className="mb-3">
-              <label className="form-label">Contraseña</label>
-              <div className="form-floating mb-3">
+              <div className="mb-3">
+                <label className="form-label">Contraseña</label>
                 <input
                   {...register("pass", {
                     required: true,
                     maxLength: 50,
                   })}
                   className="form-control"
-                  placeholder="Password"
                 />
                 {errors?.pass?.type === "maxLength" && (
                   <p className="error">
@@ -98,16 +92,12 @@ const CrearUsuario = () => {
                 {errors?.pass?.type === "required" && (
                   <p className="error">Este campo es requerido</p>
                 )}
-                <label for="floatingInput">Password</label>
               </div>
-            </div>
-            <div className="mb-3">
-              <label className="form-label">Nombre</label>
-              <div className="form-floating">
+              <div className="mb-3">
+                <label className="form-label">Nombre</label>
                 <input
                   type="text"
                   className="form-control"
-                  placeholder="Nombres"
                   {...register("name", {
                     required: true,
                     maxLength: 100,
@@ -120,59 +110,94 @@ const CrearUsuario = () => {
                 {errors?.name?.type === "pattern" && (
                   <p className="error">Solamente son permitidas letras</p>
                 )}
-                <label for="floatingInput">Nombre(s)</label>
               </div>
-              
-            </div>
-            {/* <div className="mb-3">
-              <label className="form-label">Apellidos</label>
-              <input
-                type="text"
-                className="form-control"
-                {...register("name", {
-                  required: true,
-                  maxLength: 100,
-                  pattern: /^[a-zA-Z]/,
-                })}
-              />
-              {errors?.email?.type === "required" && (
-                <p className="error">Este campo es requerido</p>
-              )}
-              {errors?.email?.type === "pattern" && (
-                <p className="error">Solamente son permitidas letras</p>
-              )}
-            </div>
-            <div className="mb-3">
-              <label className="form-label">Fecha de Nacimiento</label>
-              <input type="date" className="form-control" required />
-            </div>
-            <div className="mb-3">
-              <label className="form-label">Hobby</label>
-              <input type="text" className="form-control" />
-            </div>
-            <div className="mb-3">
-              <label className="form-label">Puesto</label>
-              <input type="text" className="form-control" required />
-            </div>
-            <div className="mb-3">
-              <label className="form-label">Habilidad</label>
-              <input type="text" className="form-control" />
-            </div>
-            <div className="mb-3">
-              <label className="form-label">Ingresa el link de tu Imagen</label>
-              <input type="text" className="form-control" />
-            </div> */}
-
-            <div className="d-grid gap-2 col-12 mx-auto">
-              <button className="btn btn-primary" type="submit" id="btnLogin">
-                Registro
-              </button>
-            </div>
-          </form>
+              <div className="mb-3">
+                <label className="form-label">Apellidos</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  {...register("last_name", {
+                    required: true,
+                    maxLength: 100,
+                    pattern: /^[a-zA-Z]/,
+                  })}
+                />
+                {errors?.last_name?.type === "required" && (
+                  <p className="error">Este campo es requerido</p>
+                )}
+                {errors?.last_name?.type === "pattern" && (
+                  <p className="error">Solamente son permitidas letras</p>
+                )}
+              </div>
+              <div className="mb-3">
+                <label className="form-label">Fecha de Nacimiento</label>
+                <input
+                  type="date"
+                  className="form-control"
+                  {...register("brd_date", {
+                    required: true,
+                  })}
+                />
+                {errors?.date?.type === "required" && (
+                  <p className="error">Este campo es requerido</p>
+                )}
+              </div>
+              <div className="mb-3">
+                <label className="form-label">Hobby</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  {...register("hobby", {
+                    required: true,
+                  })}
+                />
+                {errors?.hobby?.type === "required" && (
+                  <p className="error">Este campo es requerido</p>
+                )}
+              </div>
+              <div className="mb-3">
+                <label className="form-label">Puesto</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  {...register("puesto", {
+                    required: true,
+                  })}
+                />
+                {errors?.puesto?.type === "required" && (
+                  <p className="error">Este campo es requerido</p>
+                )}
+              </div>
+              <div className="mb-3">
+                <label className="form-label">Habilidad</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  {...register("habilidad", {
+                    required: false,
+                  })}
+                />
+              </div>
+              <div className="mb-3">
+                <label className="form-label">Ingresa el link de tu Imagen</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  {...register("img", {
+                    required: false,
+                  })}
+                />
+              </div>
+              <div className="d-grid gap-2 col-12 mx-auto">
+                <button className="btn btn-primary" type="submit" id="btnLogin">
+                  Registro
+                </button>
+              </div>
+            </form>
+          </div>
         </div>
-      </div>
-    </>
-  );
-};
-
-export { CrearUsuario };
+      </>
+    );
+  };
+  
+  export { CrearUsuario };
