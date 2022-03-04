1b3d7e55d27587bccab17f4d481bc9b30466b63a
Mariano Estrada <mariano-e-s@hotmail.com>
2022-03-04 09:24:49 -0600

    prueba

--- backend/view/user.js
+++ backend/view/user.js
@@ -1,13 +1,16 @@
 const userController = require("../controller/user");
+const auth = require("../midd/authenticate");
 
 module.exports = (app) => {
   app.post("/login", async (req, res) => {
     let result = await userController.loginUser(req.body);
     res.send(result);
   });
+
+  app.post("/whoiam",auth.authenticate,async (req, res) => {
+    let result = await userController.whoiam(req.body);
+    res.send(result);
+  });
+
 };
 
-app.post("/whoiam",auth.authenticate,async (req, res) => {
-  let result = await userController.whoiam(req.body);
-  res.send(result);
-});
